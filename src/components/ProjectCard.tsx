5a22c1059627680f39b559367938408df6718adf
rynkovski <118986014+rynkovski@users.noreply.github.com>
2023-12-12 22:04:57 +0100

    fix: project card component

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,22 +1,34 @@
-type Image = any;
+type Image = {
+  image?: string;
+  title?: string;
+  text?: string;
+  linkCode?: string;
+  linkLive?: string;
+};
 
-function ProjectCard({ prop }: Image) {
+function ProjectCard({ image, title, text, linkCode, linkLive }: Image) {
   return (
-    <div className="border w-80 h-auto my-2 p-4 flex flex-col">
-      <img className="border w-full  h-20" src={prop} alt="" />
-      <h2 className="my-2 text-xl">Title</h2>
-      <p className=" text-zinc-400 text-sm text-justify">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, esse.
-        Ex sapiente beatae consequuntur molestiae? Earum doloribus esse laborum
-        beatae eum ad ipsam omnis a, ea dolorem libero dignissimos natus.
-      </p>
+    <div className="border w-80 h-[400px] my-2 p-4 flex justify-between flex-col">
+      <img className="border w-full aspect-[4:3]" src={image} alt="" />
+      <h2 className="my-2 text-xl">{title}</h2>
+      <p className=" text-zinc-400 text-sm text-justify">{text}</p>
       <div className="flex justify-around items-center mt-2">
-        <button className="text-sky-700 border border-sky-700 rounded-xl py-3 px-6 text-center focus:bg-slate-500 hover:text-zinc-100 hover:bg-sky-500 my-2 mx-auto">
+        <a
+          href={linkCode}
+          className={`text-sky-700 border border-sky-700 rounded-xl py-3 px-6 text-center focus:bg-slate-500 hover:text-zinc-100 hover:bg-sky-500 my-2 mx-auto      ${
+            linkCode ? "cursor-pointer" : "cursor-not-allowed"
+          }`}
+        >
           Code
-        </button>
-        <button className="text-zinc-100 bg-sky-700 rounded-xl py-3 px-6 text-center focus:bg-slate-500 hover:bg-sky-500 my-2 mx-auto">
+        </a>
+        <a
+          href={linkLive}
+          className={`text-zinc-100 bg-sky-700 rounded-xl py-3 px-6 text-center focus:bg-slate-500 hover:bg-sky-500 my-2 mx-auto  ${
+            linkLive ? "cursor-pointer" : "cursor-not-allowed"
+          }`}
+        >
           Live
-        </button>
+        </a>
       </div>
     </div>
   );
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,12 +2,42 @@ import ProjectCard from "./ProjectCard";
 
 function Projects() {
   return (
-    <div id="projects" className="flex flex-col items-center py-12">
-      <h2 className="font-bold text-3xl my-8"> Projects</h2>
+    <div id="projects" className="flex flex-col items-center py-8 scroll-mt-10">
+      <h2 className="font-bold text-3xl my-2"> Projects</h2>
       <div className="flex flex-col justify-evenly items-center md:flex-row gap-8 flex-wrap">
-        <ProjectCard image={"./portfolio_project.png"} />
-        <ProjectCard />
-        <ProjectCard />
+        <ProjectCard
+          image={"./src/assets/portfolio-website.png"}
+          title="Portfolio"
+          text="This is my portfolio website. I have used React, TailwindCSS and Typescript."
+          linkCode="https://github.com/rynkovski/portfolio"
+          linkLive="https://karolrynkowski.vercel.app/"
+        />
+        <ProjectCard
+          image={"./src/assets/gearwheel-frontend.png"}
+          title="Gearwheel"
+          text="This is my cycling store website. I have used shadcn/ui library with React, TailwindCSS and Typescript."
+          linkCode="https://github.com/KITHProject/gearwheel-frontend/tree/develop_v1"
+          linkLive="https://gearwheel-frontend.vercel.app/"
+        />
+        <ProjectCard
+          image={"./src/assets/rest-countries-api.png"}
+          title="Rest Countries API"
+          text="This is my solution to Frontend Mentor Challenge. I have used React, TailwindCSS and Typescript."
+          linkCode="https://github.com/rynkovski/rest-countries-api"
+        />
+        <ProjectCard
+          image={"./src/assets/task-management.png"}
+          title="Task Management App"
+          text="This is kanban board type project (Trello clone). I have used React, TailwindCSS and Typescript."
+          linkCode="https://github.com/rynkovski/task-management"
+        />
+        <ProjectCard
+          image={"./src/assets/snake-game.png"}
+          title="Snake Game"
+          text="Simpe snake game made in vanilla javascript"
+          linkCode="https://github.com/rynkovski/snake-game"
+          linkLive="https://snake-game-rynkowskis-projects.vercel.app/"
+        />
       </div>
     </div>
   );
