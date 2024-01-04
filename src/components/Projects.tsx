12e9c597fa9bdec3d02ee35adec9928942108d03
rynkovski <118986014+rynkovski@users.noreply.github.com>
2024-01-04 20:33:59 +0100

    update projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,43 +1,55 @@
 import ProjectCard from "./ProjectCard";
 
+const ProjectsData = [
+  {
+    image: "./images/gearwheel-frontend.png",
+    title: "Gearwheel",
+    text: "Admin panel for cycling store.",
+    badges: [
+      "React",
+      "TypeScript",
+      "TailwindCSS",
+      "shadcn/ui",
+      "zustand",
+      "React Query",
+    ],
+    linkCode:
+      "https://github.com/KITHProject/gearwheel-frontend/tree/develop_v1",
+    linkLive: "https://gearwheel-frontend.vercel.app/",
+  },
+  {
+    image: "./images/rest-countries-api.png",
+    title: "Rest Countries API",
+    text: "This is my solution to Frontend Mentor Challenge.",
+    badges: ["React", "TypeScript", "TailwindCSS"],
+    linkCode: "https://github.com/rynkovski/rest-countries-api",
+  },
+  // {
+  //   image: "./images/task-management.png",
+  //   title: "Task Management App",
+  //   text: "Trello clone project on which I'm currently working on.",
+  //   badges: ["React", "TypeScript", "TailwindCSS"],
+  // },
+];
+
 function Projects() {
   return (
     <div id="projects" className="flex flex-col items-center py-8 scroll-mt-10">
-      <h2 className="font-bold text-3xl my-2"> Projects</h2>
-      <div className="flex flex-col justify-evenly items-center md:flex-row gap-8 flex-wrap">
-        <ProjectCard
-          image={"./images/portfolio-website.png"}
-          title="Portfolio"
-          text="This is my portfolio website. I have used React, TailwindCSS and Typescript."
-          linkCode="https://github.com/rynkovski/portfolio"
-          linkLive="https://karolrynkowski.vercel.app/"
-        />
-        <ProjectCard
-          image={"./images/gearwheel-frontend.png"}
-          title="Gearwheel"
-          text="This is my cycling store website. I have used shadcn/ui library with React, TailwindCSS and Typescript."
-          linkCode="https://github.com/KITHProject/gearwheel-frontend/tree/develop_v1"
-          linkLive="https://gearwheel-frontend.vercel.app/"
-        />
-        <ProjectCard
-          image={"./images/rest-countries-api.png"}
-          title="Rest Countries API"
-          text="This is my solution to Frontend Mentor Challenge. I have used React, TailwindCSS and Typescript."
-          linkCode="https://github.com/rynkovski/rest-countries-api"
-        />
-        <ProjectCard
-          image={"./images/task-management.png"}
-          title="Task Management App"
-          text="This is kanban board type project (Trello clone). I have used React, TailwindCSS and Typescript."
-          linkCode="https://github.com/rynkovski/task-management"
-        />
-        <ProjectCard
-          image={"./images/snake-game.png"}
-          title="Snake Game"
-          text="Simpe snake game made in vanilla javascript"
-          linkCode="https://github.com/rynkovski/snake-game"
-          linkLive="https://snake-game-rynkowskis-projects.vercel.app/"
-        />
+      <h2 className="my-2 text-3xl font-bold"> Projects</h2>
+      <div className="flex flex-col flex-wrap items-center gap-8 justify-evenly md:flex-row">
+        {ProjectsData.map((project) => {
+          return (
+            <ProjectCard
+              key={project.title}
+              image={project.image}
+              title={project.title}
+              text={project.text}
+              badges={project.badges}
+              linkCode={project.linkCode}
+              linkLive={project.linkLive}
+            />
+          );
+        })}
       </div>
     </div>
   );
