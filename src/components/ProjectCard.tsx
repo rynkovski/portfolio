f92a3e920e9bbb8473a047b706b04acf5d87f1a7
rynkovski <118986014+rynkovski@users.noreply.github.com>
2024-01-04 20:34:12 +0100

    feat: add badge in project

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,32 +1,52 @@
-type Image = {
+import Badge from "./ui/badge";
+
+type ProjectCard = {
   image?: string;
   title?: string;
   text?: string;
+  badges: string[];
   linkCode?: string;
   linkLive?: string;
 };
 
-function ProjectCard({ image, title, text, linkCode, linkLive }: Image) {
+function ProjectCard({
+  image,
+  title,
+  text,
+  badges,
+  linkCode,
+  linkLive,
+}: ProjectCard) {
   return (
     <div className="border w-80 h-[400px] my-2 p-4 flex justify-between flex-col">
       <a href={linkLive}>
         <img className="border w-full aspect-[4:3]" src={image} alt="" />
       </a>
       <h2 className="my-2 text-xl">{title}</h2>
-      <p className=" text-zinc-400 text-sm text-justify">{text}</p>
-      <div className="flex justify-around items-center mt-2">
+      <p className="text-sm text-justify text-zinc-400">{text}</p>
+      <div className="flex flex-wrap gap-1">
+        {badges.map((badge) => {
+          return <Badge key={badge}>{badge}</Badge>;
+        })}
+      </div>
+
+      <div className="flex items-center justify-around mt-2">
         <a
           href={linkCode}
-          className={`text-sky-700 border border-sky-700 rounded-xl py-3 px-6 text-center focus:bg-slate-500 hover:text-zinc-100 hover:bg-sky-500 my-2 mx-auto      ${
-            linkCode ? "cursor-pointer" : "cursor-not-allowed"
+          className={`border shadow-md  border-sky-700 rounded-xl py-3 px-6 text-center focus:bg-sky-800  my-2 mx-auto      ${
+            linkCode
+              ? "cursor-pointer text-sky-700  hover:text-zinc-100 hover:bg-sky-500"
+              : "cursor-not-allowed text-black bg-zinc-100  border-black"
           }`}
         >
           Code
         </a>
         <a
           href={linkLive}
-          className={`text-zinc-100 bg-sky-700 rounded-xl py-3 px-6 text-center focus:bg-slate-500 hover:bg-sky-500 my-2 mx-auto  ${
-            linkLive ? "cursor-pointer" : "cursor-not-allowed"
+          className={` shadow-md border border-sky-700 bg-sky-700 rounded-xl py-3 px-6 text-center focus:bg-sky-800  my-2 mx-auto  ${
+            linkLive
+              ? "cursor-pointer text-zinc-100 hover:bg-sky-500"
+              : "cursor-not-allowed text-black bg-zinc-100  border-black"
           }`}
         >
           Live
