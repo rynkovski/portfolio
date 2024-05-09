2dbc6006ed41ea91f8fdd6d822df94b3c1033432
rynkovski <118986014+rynkovski@users.noreply.github.com>
2024-05-09 23:25:47 +0200

    feat: change hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,26 +1,26 @@
 function Hero() {
   return (
-    <div className="flex flex-col-reverse sm:flex-row sm:justify-around items-center border-b-2 pt-24 pb-8 px-8 gap-8 sm:gap-0">
+    <div className="flex flex-col-reverse items-center gap-8 px-8 pt-24 pb-8 border-b-2 sm:flex-row sm:justify-around sm:gap-0">
       <div className="flex flex-col items-center justify-center text-center sm:w-1/2">
         <p className="text-2xl font-semibold sm:text-3xl md:text-4xl">
           Hello everyone! 👋🏻
         </p>
-        <h2 className="text-xl sm:text-2xl text-zinc-500">
+        <h2 className="text-xl sm:text-2xl text-slate-500">
           I'm Karol Rynkowski, a passionate self-taught frontend developer!
         </h2>
-        <p className="text-zinc-400">
+        <p className="text-slate-400">
           Scroll down to learn more about me and my projects!
         </p>
         <a
           href="#contact"
-          className="text-zinc-100 bg-sky-700 rounded-xl py-3 px-6 text-center focus:bg-zinc-500 hover:bg-sky-500 my-2 mx-auto"
+          className="px-6 py-3 mx-auto my-2 text-center bg-sky-600 text-stone-50 rounded-xl focus:bg-sky-800 hover:bg-sky-500"
         >
           Contact me
         </a>
       </div>
-      <div className="w-64 relative flex items-center justify-center">
+      <div className="relative flex items-center justify-center w-64">
         <img
-          className="rounded-3xl shadow-lg"
+          className="shadow-lg rounded-3xl"
           src="./images/photo.jpg"
           alt="Karol's photo"
         />
